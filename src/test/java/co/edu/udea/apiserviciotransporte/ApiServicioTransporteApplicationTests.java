01ea011131c673dcf0bdfb35fe6b2fa2fcb250a0
Juan Camilo Cardona Calderón <juancamilo19997814@gmail.com>
2023-11-02 19:41:58 -0500

    Initial commit

---
+++ src/test/java/co/edu/udea/apiserviciotransporte/ApiServicioTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.apiserviciotransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServicioTransporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
